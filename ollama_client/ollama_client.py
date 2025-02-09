fbf5ebfd8e8b32232620bfbf978c643e70f3a854
CC29 <chih.chiu.19@gmail.com>
2025-02-09 14:21:11 -0800

    add streaming api

--- ollama_client/ollama_client.py
+++ ollama_client/ollama_client.py
@@ -3,8 +3,10 @@ from tenacity import retry, stop_after_attempt, wait_exponential, retry_if_excep
 
 import ollama
 
+from typing import Iterable
+
 # Remember to start the server using the command line; using GUI sometimes makes the server undiscoverable on lan.
-OLLAMA_HOST = '192.168.1.244:11434'
+OLLAMA_HOST = '1ocalhost:11434'
 
 _CACHE = {}
 
@@ -21,8 +23,19 @@ def client():
 
 @retry(stop=stop_after_attempt(3), wait=wait_exponential(multiplier=1), retry=retry_if_exception_type(ReadTimeout))
 def quickchat(model: str, messages: list) -> str:
+  # Messages can be like [{"role": "user", "content": "Why is the sky blue?"}]
   response = client().chat(
     model=model,
     messages=messages,
   )
   return response['message']['content']
+
+
+def streamchat(model: str, messages: list) -> Iterable:
+  stream = client().chat(
+    model=model,
+    messages=messages,
+    stream=True,
+  )
+  for chunk in stream:
+    yield chunk["message"]["content"]
